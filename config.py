b3f7e805d1b497e95362b5e11084a1e5a7a315af
Yesmelin Perdomo <yesmelin.perdomo@gmail.com>
2024-07-18 20:46:39 -0400

    loading sodapy api

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 #  Add your API key
 api_key = 'ac9fac87d6b0458e95e262a624e45b44'
-fromdate = '01-31-2024'
-todate = '03-20-2024'
+fromdate = '1/1/2020'
+todate = '2/1/2020'
+# par311 = 'I5qvfq5xKzME0DL0ps6xSsRtY'
+# username ='yesmelin.perdomo@gmail.com'
+# password = 'NYCopendataproject3#'
